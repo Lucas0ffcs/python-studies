496526e0dff34c39539d89372dd442e35f6111d3
Lucas0ffcs <lacerdalucas511@gmail.com>
2025-11-26 13:19:00 -0300

    Optei por usar join por se tratar de uma lista com os caractéres do texto digitado pelo usuário

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,14 +1,16 @@
-text = input("Enter the text to be encrypted:")
-key = int(input("Enter the key of the cypher:"))
-cypher = []
+text = input("Enter the text to be encrypted:\n")
+key = int(input("Enter the key of the cypher:\n"))
+cypherlist = []
 
 for ch in text:
     cont = 0
     if ch.isdigit() or ch.isspace():
-       continue
-    cypher[cont].append(chr(ord(ch)+key))
-
-
+        cypherlist[cont].append(ch)
+        continue
+    cypherlist[cont].append(chr(ord(ch)+key))
+    cont += 1
+cypher = "".join(cypherlist)
+print(cypher)
 
 
 
