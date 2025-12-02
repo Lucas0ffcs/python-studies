a9fb326a5e07d48359f76ed5681776ad969b68ae
Lucas0ffcs <lacerdalucas511@gmail.com>
2025-12-02 12:26:18 -0300

    Melhora função stringer()

--- classes1/timer.py
+++ classes1/timer.py
@@ -26,11 +26,30 @@ class Timer:
         if self.__hour == 24:
             self.__hour = 0
 
+    def prev_second(self):
+        self.__sec -= 1
+        if self.__sec < 0:
+            self.__sec = 59
+            self.__min -= 1
+
+        if self.__min < 0:
+            self.__min = 59
+            self.__hour -= 1
+
+        if self.__hour < 0:
+            self.__hour = 23
 
 
+    def getHour(self):
+        return self.__hour
+    def getMin(self):
+        return self.__min
+    def getSec(self):
+        return self.__sec
 
 def stringer(hour, min, sec):
-    print(hour + ":" + min + ":" +  sec)
+    string = f" {hour} + : + {min} + : +  {sec}"
+    print(string)
 
 time  = Timer(15, 12, 30)
-stringer(time.__hour)
+stringer(time.getHour() + ":" + time.getMin() + ":" + time.getSec())
