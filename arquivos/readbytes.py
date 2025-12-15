eb366cffc1a36001114a6f80e43eef53c1ddfb0f
Lucas0ffcs <lacerdalucas511@gmail.com>
2025-12-15 20:11:29 -0300

    Lê os bytes diretamente do arquivo bytearray sem precisar de um objeto secundario como quando se usa readinto

---
+++ arquivos/readbytes.py
@@ -0,0 +1,12 @@
+from os import strerror
+
+try:
+    binary_file = open('file.bin', 'rb')
+    data = bytearray(binary_file.read())
+    binary_file.close()
+
+    for b in data:
+        print(hex(b), end=' ')
+
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
