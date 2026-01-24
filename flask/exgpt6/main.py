ef08d0e9453dfdfda5af7bbac82fa41a2e369f26
Lucas0ffcs <lacerdalucas511@gmail.com>
2026-01-24 18:53:24 -0300

    Finaliza exercicio de pagina de signup e login, muitos erros ainda presentes

--- flask/exgpt6/main.py
+++ flask/exgpt6/main.py
@@ -1,8 +1,10 @@
+#main.py
 from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
+usuarios = {}
 
 @app.route('/')
 def index():
@@ -11,19 +13,32 @@ def index():
 
 @app.route('/signup', methods=["GET","POST"])
 def signup():
-    usuario = request.form["usuario"]
-    senha = request.form["senha"]
-
     if request.method == "POST":
-        usuario = request.form["usuario"]
-        senha = request.form['senha']
+        usuario = request.form.get("usuario")
+        senha = request.form.get("senha")
+        if (usuario,senha) not in usuarios.items():
+            usuarios[usuario] = senha
+        else:
+            return render_template('signup.html',jacadastrado="Usuário já cadastrado!",printer = usuarios)
+
+        
     
-    return render_template('index.html', usuario = usuario, senha=senha)
+        return render_template("cadastrado.html",printer = usuarios)
+
+    return render_template("signup.html", printer = usuarios)   
 
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        usuario = request.form.get("usuario")
+        senha = request.form.get("senha")
+        if (usuario,senha) in usuarios.items():
+            return render_template('logado.html')
+        else:
+            return render_template('login.html',negado="Usuário ou senha inválidos!!!")
+
     return render_template('login.html')
 
 
